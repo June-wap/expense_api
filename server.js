c9cdebe26135e3f5e7ee02f3f06a84a42a2f0770
June-wap <anhquang657@gmail.com>
2025-12-01 12:00:54 +0700

    Fix dotenv import for Render

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
+import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
-import dotenv from "dotenv/lib/main.js";
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
